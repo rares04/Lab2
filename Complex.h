8c2c8e6eefa72690321fe3c75e685987ba4f79e5
rares04 <raresastilean@yahoo.com>
2020-03-09 19:27:14 +0200

    Am adaugat primu file coaie

---
+++ Complex.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Complex {
+private:
+	double real, imag;
+
+public:
+	
+	// Constructor
+	Complex(double real, double imag);
+
+	// Returns the value of the real part of the complex number
+	double getReal() const;
+
+	// Returns the value of the imaginary part of the complex number
+	double getImag() const;
+
+	// Changes the value of the real part of the complex number
+	void setReal(double real);
+
+	// Changes the value of the imaginary part of the complex number
+	void setImag(double imag);
+};
